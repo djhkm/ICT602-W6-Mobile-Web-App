6701220f34699a0335e1612228c38e13aae934f6
Haziq Aiman <haziqaiman2@gmail.com>
2021-11-30 18:59:57 +0800

    navbar.js addition

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+var element = document.getElementsByClassName('android-navigation-container');
+
+element[0].innerHTML = '<nav class=\"android-navigation mdl-navigation\">' + 
+              '<a class=\"mdl-navigation__link mdl-typography--text-uppercase\" href=\"index.html\">Home</a>' + 
+              '<a class=\"mdl-navigation__link mdl-typography--text-uppercase\" href=\"about.html\">About</a>' + 
+              '<a class=\"mdl-navigation__link mdl-typography--text-uppercase\" href=\"track.html\">Track my parcel</a>' + 
+              '<a class=\"mdl-navigation__link mdl-typography--text-uppercase\" href=\"shipping.html\">Shipping</a>' + 
+              '<a class=\"mdl-navigation__link mdl-typography--text-uppercase\" href=\"contact.html\">Contact</a>' + 
+			'</nav>';
